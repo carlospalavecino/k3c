eaef9f4cf8a6931c438f5f279191a1d7a1d05869
Carlos Palavecino <info@ueii.net>
2023-08-08 18:59:04 -0300

    add init cmd

---
+++ src/command/init.sh
@@ -0,0 +1,4 @@
+echo "# this file is located in 'src/command/init.sh'"
+echo "# code for 'k3c init' goes here"
+echo "# you can edit it freely and regenerate (it will not be overwritten)"
+inspect_args
